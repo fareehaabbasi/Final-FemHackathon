5cd105df99c13764b786e6215c043e7105d2bfb1
fareehaabbasi <abbasifareeha9@gmail.com>
2026-09-01 03:23:48 +0500

    change

--- Frontend/src/feature/auth/components/Protected.jsx
+++ Frontend/src/feature/auth/components/Protected.jsx
@@ -1,19 +1,23 @@
 import { useAuth } from "../hook/useAuth";
 import { Navigate } from "react-router";
-import React from 'react'
+import React from "react";
 
-const Protected = ({children}) => {
-    const {loading, user} = useAuth()
+const Protected = ({ children }) => {
+    const { loading, user } = useAuth();
 
-    if(loading) {
-        return (<main><h1>Loading...........</h1></main>)
+    if (loading) {
+        return (
+            <main className="min-h-screen bg-[#0b0b0b] text-white flex items-center justify-center">
+                <h1>Loading...........</h1>
+            </main>
+        );
     }
 
-    if(!user) {
-        return <Navigate to={'/login'} />
+    if (!user) {
+        return <Navigate to="/login" replace />;
     }
 
-  return children
-}
+    return children;
+};
 
-export default Protected
+export default Protected;
